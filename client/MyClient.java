4c9d5ce1a1ef63a801a7f457720b176bd6c5ed1c
Dylan Lederle-Ensign <dylan.lederle.ensign@gmail.com>
2013-03-19 09:33:47 -0400

    Initial Client

---
+++ client/MyClient.java
@@ -0,0 +1,70 @@
+/**
+ * Dave Toth (Adapted from Sun's Java Tutorial code)
+ * This program is the client portion of a client-server pair.
+ */
+
+import java.io.*;
+import java.net.*;
+
+public class MyClient {
+
+  public static void main(String[] args) throws IOException {
+
+    Socket mySocket = null;
+    PrintWriter out = null;
+    BufferedReader in = null;
+
+    try {
+      mySocket = new Socket("rosemary.umw.edu", 2301);
+      out = new PrintWriter(mySocket.getOutputStream(), true);
+      in = new BufferedReader(new InputStreamReader(mySocket.getInputStream()));
+    } 
+    catch (UnknownHostException e) {
+      System.err.println("I couldn't find the host rosemary.umw.edu.");
+      System.exit(1);
+    } 
+    catch (IOException e) {
+      System.err.println("I/O exception with rosemary.umw.edu.");
+      System.err.println(e);
+      System.exit(1);
+    }
+    
+    boolean gameOn = false;
+    while(!gameOn) {
+      System.out.println("Ready to play? (y/n)");
+      String line = getUserIn();
+      if(line.equals("y")) {
+        gameOn = true;
+      }
+
+
+      while(gameOn) {
+        String fromServer;
+        out.println("I wanna play!");
+        boolean playing = true;
+        while(playing) {
+          fromServer = in.readLine();
+          System.out.println(fromServer);
+          String response = getUserIn();
+          out.println(response);
+        }
+        gameOn = false;
+      }
+    }
+    System.out.println("closing socket!!!!!"); 
+    out.close();
+    in.close();
+    mySocket.close();
+  }
+
+  private static String getUserIn() {
+    try {
+      BufferedReader userIn = new BufferedReader(new InputStreamReader(System.in));
+      return userIn.readLine();
+    } catch(Exception e) {
+      System.err.println("Error!");
+      System.err.println(e);
+    }
+    return "Error";
+  }
+}
